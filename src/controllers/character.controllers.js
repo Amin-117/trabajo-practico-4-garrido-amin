4c5262a890e901f426309957e3d4c01327cdce3a
Garrido Amin <garridoamin@gmail.com>
2025-06-23 11:39:26 -0300

    Realizacion de los .controladoress

--- src/controllers/character.controllers.js
+++ src/controllers/character.controllers.js
@@ -1,2 +1,24 @@
+import { request } from "express";
 import Character from "../models/character.model";
+import { UniqueConstraintError, ValidationError } from "sequelize";
 
+export const createCharacter = async (requestAnimationFrame, res) => {
+    try{
+        const character = await Character.create(req.body);
+        res.status(201).json(character);
+    
+} catch (error) {
+    if (error instanceof UniqueConstraintError) {
+        return res.status(400).json({
+            messege: "ya existe un personaje con ese nombre",
+        });
+    }
+}
+if (error instanceof ValidationError){
+    const errors = error.errors.mep(err => ({
+        message: err.messege
+    }));
+    return res.status(404).json({
+        Message: "erro de validacion de datos"
+    })
+}};
